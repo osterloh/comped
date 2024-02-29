467101dd6a6a7d3ed670b59dcdc275ab8850b353
Osterloh <johnatanosterloh@gmail.com>
2024-02-29 20:33:40 -0300

    Configuração de model, repository e service para pessoa

---
+++ src/main/java/br/com/senai/model/Pessoa.java
@@ -0,0 +1,24 @@
+package br.com.senai.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private int idade;
+
+}
---
+++ src/main/java/br/com/senai/repository/PessoaRepository.java
@@ -0,0 +1,10 @@
+package br.com.senai.repository;
+
+import br.com.senai.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+}
